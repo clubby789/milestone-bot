2fb538d3cbe70d5f751a4153a50c79fab452ae39
clubby789 <jamie@hill-daniel.co.uk>
2020-02-24 19:18:22 +0000

    Merge files

--- bot.py
+++ bot.py
@@ -3,7 +3,19 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 import urllib.request
 import time
-import quote
+import requests
+import json
+def getQuote():
+	try:
+		url = "https://inspirobot.me/api?generateFlow=1"
+		r = requests.get(url)
+
+		quote = json.loads(r.text)['data'][1]['text']
+	except:
+		quote = -1
+	return quote
+
+
 try:
 	from win10toast import ToastNotifier
 	home = 1
